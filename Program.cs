dd21179daba2818d2864d332c6d30b35e258b1c2
Julian <julianintveld@live.nl>
2022-01-25 23:17:15 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LazyPassword
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var text = Clipboard.GetText();
+            SendKeys.SendWait(text);
+        }
+    }
+}
