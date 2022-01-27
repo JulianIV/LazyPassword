a9891b5ee64ca7a3f43985026449c758d1c242ff
Julian <julianintveld@live.nl>
2022-01-27 17:12:26 +0100

    Fix solution

---
+++ LazyPassword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LazyPassword
+{
+    class Program
+    {
+        private static readonly string[] _specialChars = { "{", "}", "(", ")", "+", "^" };
+
+        [STAThread]
+        private static void Main()
+        {
+            var text = Clipboard.GetText();
+            var escapedText = text;
+
+            foreach (var specialChar in _specialChars)
+            {
+                escapedText = escapedText.Replace(specialChar, $"{{{specialChar}}}");
+            }
+
+            SendKeys.SendWait(escapedText);
+        }
+    }
+}
